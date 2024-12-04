02814c760265ff3ef18e7b8f9f6a387be2d583a4
muratdayan <muratdayan359@gmail.com>
2024-12-04 12:19:16 +0300

    FastWordTokenBarComp styled

--- core/ui/src/main/java/com/muratdayan/ui/components/FastWordTokenBarComp.kt
+++ core/ui/src/main/java/com/muratdayan/ui/components/FastWordTokenBarComp.kt
@@ -1,38 +1,37 @@
 package com.muratdayan.ui.components
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
+import com.muratdayan.core_ui.ui.theme.Dimensions
 import com.muratdayan.ui.R
 import com.muratdayan.ui.theme.FastWordTheme
-import com.muratdayan.ui.theme.extendedColors
 
 @Composable
-fun FastWordEnergyBarComp(
-    currentEnergy: Int = 0,
-    maxEnergy: Int = 0,
-    modifier: Modifier = Modifier
+fun FastWordTokenBarComp(
+    tokenValue: Int = 0,
+    modifier: Modifier = Modifier,
+    @DrawableRes icon: Int = R.drawable.ic_emerald,
 ){
-
-    val energyFraction = currentEnergy.toFloat() / maxEnergy.toFloat()
-    val barColor = MaterialTheme.colorScheme.onTertiaryContainer
 
     Box(
         modifier = Modifier
@@ -47,12 +46,7 @@ fun FastWordEnergyBarComp(
                 .clip(MaterialTheme.shapes.small)
                 .background(MaterialTheme.colorScheme.scrim)
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(energyFraction)
-                    .background(barColor)
-            )
+
         }
 
         Box(
@@ -62,15 +56,16 @@ fun FastWordEnergyBarComp(
         ) {
             Box(
                 modifier = Modifier
+                    .padding(bottom = Dimensions.paddingSmall, end = Dimensions.paddingSmall)
                     .fillMaxHeight()
                     .align(Alignment.TopStart)
             ) {
                 Icon(
-                    painter = painterResource(R.drawable.ic_flash),
+                    painter = painterResource(icon),
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxHeight(),
-                    tint = MaterialTheme.colorScheme.background
+                    tint = Color.Unspecified
                 )
             }
             Box(
@@ -80,30 +75,30 @@ fun FastWordEnergyBarComp(
                 Icon(
                     painter = painterResource(R.drawable.ic_add),
                     contentDescription = null,
-                    modifier = Modifier.height(20.dp),
+                    modifier = Modifier
+                        .size(width = 30.dp, height = 25.dp),
                     tint = MaterialTheme.colorScheme.background
                 )
             }
         }
 
         FastWordTextComp(
-            text = "$currentEnergy/$maxEnergy",
+            text = tokenValue.toString(),
             modifier = Modifier
                 .align(Alignment.Center),
             color = MaterialTheme.colorScheme.background,
-            textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold
         )
     }
 }
 
+
+
 @Preview
 @Composable
-private fun FastWordEnergyBarCompPreview(){
+private fun FastWordTokenBarCompPreview(){
     FastWordTheme {
-        FastWordEnergyBarComp(
-            currentEnergy = 3,
-            maxEnergy = 10
-        )
+        FastWordTokenBarComp()
     }
+
 }
