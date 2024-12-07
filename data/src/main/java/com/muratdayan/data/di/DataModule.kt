29a1f94ae640d1a8caaa6eee5b144fb110e429e8
muratdayan <muratdayan359@gmail.com>
2024-12-07 13:19:35 +0300

    auth repository provided

--- data/src/main/java/com/muratdayan/data/di/DataModule.kt
+++ data/src/main/java/com/muratdayan/data/di/DataModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 import javax.inject.Singleton
@@ -23,6 +24,7 @@ object DataModule {
         return createSupabaseClient(supabaseUrl = supabaseUrl, supabaseKey = supabaseKey){
             install(Realtime)
             install(Postgrest)
+            install(Auth)
         }
     }
 }
