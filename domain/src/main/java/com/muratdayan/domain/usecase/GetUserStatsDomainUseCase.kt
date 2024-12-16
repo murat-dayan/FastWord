4a4da31ec5949f24c4b9199e7a78d04c75be8ce5
muratdayan <muratdayan359@gmail.com>
2024-12-16 18:23:23 +0300

    update functions added for token , energy and emerald in data module and domain module

--- domain/src/main/java/com/muratdayan/domain/usecase/GetUserStatsDomainUseCase.kt
+++ domain/src/main/java/com/muratdayan/domain/usecase/GetUserStatsDomainUseCase.kt
@@ -1,10 +1,10 @@
 package com.muratdayan.domain.usecase
 
-import com.muratdayan.domain.repository.UsersDomainRepository
+import com.muratdayan.domain.repository.UserStatsDomainRepository
 import javax.inject.Inject
 
 class GetUserStatsDomainUseCase @Inject constructor(
-    private val usersDomainRepository: UsersDomainRepository
+    private val userStatsDomainRepository: UserStatsDomainRepository
 ){
-    operator fun invoke() = usersDomainRepository.getUserStats()
+    operator fun invoke() = userStatsDomainRepository.getUserStats()
 }
