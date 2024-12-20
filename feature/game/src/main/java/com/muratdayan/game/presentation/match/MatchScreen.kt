06357278736a2e8a72ac22fde8491d1eda40ccae
muratdayan <muratdayan359@gmail.com>
2024-12-20 20:18:08 +0300

    Matchscreen styled matchviewmodel and matchcontract created and getuserinfo and findorcreateroom functions added

---
+++ feature/game/src/main/java/com/muratdayan/game/presentation/match/MatchScreen.kt
@@ -0,0 +1,137 @@
+package com.muratdayan.game.presentation.match
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.muratdayan.core_ui.ui.theme.Dimensions
+import com.muratdayan.game.R
+import com.muratdayan.ui.components.FastWordProfileImageComp
+import com.muratdayan.ui.components.FastWordTextComp
+import com.muratdayan.ui.theme.FastWordTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+@Composable
+fun MatchScreenRoot(
+    modifier: Modifier = Modifier,
+    matchViewModel: MatchViewModel
+) {
+    val uiState = matchViewModel.uiState.collectAsStateWithLifecycle()
+    val uiEffect = matchViewModel.uiEffect
+
+    MatchScreen(
+        modifier = modifier,
+        uiState = uiState.value,
+        uiEffect = uiEffect,
+        onAction = matchViewModel::onAction
+    )
+}
+
+@Composable
+private fun MatchScreen(
+    modifier: Modifier = Modifier,
+    uiState: MatchContract.UiState,
+    uiEffect: Flow<MatchContract.UiEffect>,
+    onAction: (MatchContract.UiAction) -> Unit
+) {
+
+    LaunchedEffect(true) {
+        // İlk olarak userInfo'yu al
+        onAction(MatchContract.UiAction.GetUserInfo)
+    }
+
+    LaunchedEffect(uiState.userInfo) {
+        uiState.userInfo?.let {
+            onAction(MatchContract.UiAction.FindOrCreateRoom(it.id))
+        }
+    }
+
+    Box (
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(Dimensions.paddingSmall),
+
+    ){
+
+        IconButton(
+            onClick = {}
+        ) {
+            Icon(
+                painter = painterResource(com.muratdayan.ui.R.drawable.ic_back),
+                contentDescription = "Back",
+                tint = MaterialTheme.colorScheme.background
+            )
+        }
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(Dimensions.spacingMedium)
+            ){
+                FastWordProfileImageComp(
+                    imageUri = uiState.userInfo?.avatar_uri,
+                    size = 100
+                )
+                FastWordTextComp(
+                    text = uiState.userInfo?.user_name ?: "",
+                )
+            }
+
+            FastWordTextComp(
+                text = "VS.",
+                fontWeight = FontWeight.Bold,
+                fontSize = Dimensions.textSizeTitle
+            )
+
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(Dimensions.spacingMedium)
+            ){
+                FastWordProfileImageComp(
+                    imagePainter = painterResource(com.muratdayan.ui.R.drawable.avatar),
+                    size = 100
+                )
+                FastWordTextComp(
+                    text = if (uiState.isLoading) "Waiting..." else "",
+                    color = MaterialTheme.colorScheme.secondaryContainer
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MatchScreenPreview() {
+    FastWordTheme {
+        MatchScreen(
+            modifier = Modifier,
+            onAction = {},
+            uiState = MatchContract.UiState(),
+            uiEffect = emptyFlow()
+        )
+    }
+}
---
+++ feature/game/src/main/java/com/muratdayan/game/presentation/match/MatchViewModel.kt
@@ -0,0 +1,107 @@
+package com.muratdayan.game.presentation.match
+
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.muratdayan.common.Result
+import com.muratdayan.domain.usecase.GetUserInfoDomainUseCase
+import com.muratdayan.game.domain.model.MatchResult
+import com.muratdayan.game.domain.usecase.FindOrCreateRoomUseCase
+import com.muratdayan.game.domain.usecase.StartRealtimeRoomListenerUseCase
+import com.muratdayan.game.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MatchViewModel @Inject constructor(
+    getUserInfoDomainUseCase: GetUserInfoDomainUseCase,
+    private val findOrCreateRoomUseCase: FindOrCreateRoomUseCase,
+    private val startRealtimeRoomListenerUseCase: StartRealtimeRoomListenerUseCase
+) : BaseViewModel(getUserInfoDomainUseCase){
+
+    private val _uiState = MutableStateFlow(MatchContract.UiState())
+    val uiState: StateFlow<MatchContract.UiState> = _uiState.asStateFlow()
+
+    private val _uiEffect by lazy { Channel<MatchContract.UiEffect>() }
+    val uiEffect: Flow<MatchContract.UiEffect> by  lazy { _uiEffect.receiveAsFlow() }
+
+    fun onAction(action: MatchContract.UiAction){
+        when(action){
+            MatchContract.UiAction.GetUserInfo -> {
+                getUserInfo()
+            }
+
+            is MatchContract.UiAction.FindOrCreateRoom -> {
+                findOrCreateRoom(action.userId)
+            }
+        }
+
+    }
+
+    private fun getUserInfo(){
+        viewModelScope.launch {
+            val userInfo = fetchUserInfo()
+
+            // userInfo null değilse, FindOrCreateRoom aksiyonunu başlatıyoruz
+            if (userInfo != null) {
+                updateUiState { copy(userInfo = userInfo) }
+            }
+        }
+    }
+
+    private fun findOrCreateRoom(userId:String){
+        viewModelScope.launch {
+            updateUiState { copy(isLoading = true) }
+            findOrCreateRoomUseCase.invoke(userId).collect { result ->
+                when (result) {
+                    is Result.Error -> {
+                        updateUiState { copy(isLoading = false) }
+                    }
+                    is Result.Success -> {
+                        when(result.data){
+                            MatchResult.NoAvailableRoom -> {
+                                updateUiState { copy(isLoading = false) }
+                                Log.d("MatchViewModel", "findOrCreateRoom: NoAvailableRoom")
+                            }
+                            is MatchResult.RoomCreated -> {
+                                Log.d("MatchViewModel", "findOrCreateRoom: RoomCreated")
+                                (result.data as MatchResult.RoomCreated).room.id?.let {
+                                    startRealtimeRoomListenerUseCase.invoke(
+                                        it
+                                    ).onEach { room->
+                                        Log.d("MatchViewModel", "findOrCreateRoom: $room")
+                                        updateUiState { copy(isLoading = false, room = room) }
+                                    }.launchIn(viewModelScope)
+                                }
+                            }
+                            is MatchResult.RoomFound -> {
+                                Log.d("MatchViewModel", "findOrCreateRoom: RoomFound")
+                                updateUiState { copy(isLoading = false, room = (result.data as MatchResult.RoomFound).room) }
+                                emitUiEffect(MatchContract.UiEffect.NavigateToPlayScreen)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun updateUiState(block: MatchContract.UiState.() -> MatchContract.UiState){
+        _uiState.update(block)
+    }
+
+    private suspend fun emitUiEffect(uiEffect: MatchContract.UiEffect){
+        _uiEffect.send(uiEffect)
+    }
+
+}
