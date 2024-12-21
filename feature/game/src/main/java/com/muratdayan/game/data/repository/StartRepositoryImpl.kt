beec845bc385525854b2ad252a8b665930afd535
muratdayan <muratdayan359@gmail.com>
2024-12-22 02:19:45 +0300

    add getrandomquestion func added in StartRepository and StartRepositoryImpl

--- feature/game/src/main/java/com/muratdayan/game/data/repository/StartRepositoryImpl.kt
+++ feature/game/src/main/java/com/muratdayan/game/data/repository/StartRepositoryImpl.kt
@@ -4,10 +4,13 @@ import android.util.Log
 import com.muratdayan.common.AppError
 import com.muratdayan.common.DataError
 import com.muratdayan.common.Result
+import com.muratdayan.game.domain.model.QuestionModel
 import com.muratdayan.game.domain.model.RoomModel
 import com.muratdayan.game.domain.repository.StartRepository
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.postgrest.rpc
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -36,6 +39,30 @@ class StartRepositoryImpl @Inject constructor(
             emit(Result.Error(DataError.Remote.ServerError))
 
         }
+    }
+
+    override fun getRandomQuestion(): Flow<Result<QuestionModel, AppError>> = flow {
+
+        try {
+            val response = supabaseClient
+                .postgrest
+                .rpc("get_random_question")
+
+            Log.e("StartRepositoryImpl","getRandomQuestion: $response")
+
+            val question = response.decodeSingleOrNull<QuestionModel>()
+
+            if (question != null) {
+                emit(Result.Success(question))
+            } else {
+                Log.e("StartRepositoryImpl", "getRandomQuestion: Question not found")
+                emit(Result.Error(DataError.Remote.ServerError))
+            }
+        }catch (e:Exception){
+            Log.e("StartRepositoryImpl","getRandomQuestion: ${e.message}")
+            emit(Result.Error(DataError.Remote.ServerError))
+        }
+
     }
 
 
--- feature/game/src/main/java/com/muratdayan/game/domain/repository/StartRepository.kt
+++ feature/game/src/main/java/com/muratdayan/game/domain/repository/StartRepository.kt
@@ -2,10 +2,13 @@ package com.muratdayan.game.domain.repository
 
 import com.muratdayan.common.AppError
 import com.muratdayan.common.Result
+import com.muratdayan.game.domain.model.QuestionModel
 import com.muratdayan.game.domain.model.RoomModel
 import kotlinx.coroutines.flow.Flow
 
 interface StartRepository {
 
     fun getRoom(roomId:String): Flow<Result<RoomModel,AppError>>
+
+    fun getRandomQuestion(): Flow<Result<QuestionModel,AppError>>
 }
